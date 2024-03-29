78774465f4071cce3e5a86bc58d475f5f8940ddf
onurceymen <onurceymen@yahoo.com>
2024-03-30 00:10:14 +0300

    bugs fixed

--- PokemonAPI/PokemonReviewApp/Dto/ReviewerDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PokemonReviewApp.Dto
-{
-    public class ReviewerDto
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
